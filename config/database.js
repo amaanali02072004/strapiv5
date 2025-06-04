dda628c96f4d53bf4083cb897a7d6ede31fc9109
Amaan Ali <killeramaan12345@gmail.com>
2025-06-04 15:02:20 +0530

    testing render.com development

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => {
 
   const connections = {
     mysql: {
+    client: 'postgres',
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
@@ -17,7 +18,7 @@ module.exports = ({ env }) => {
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
         },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
